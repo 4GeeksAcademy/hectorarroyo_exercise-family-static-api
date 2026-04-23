ec4d38e0fedd15f207a8dfba566ffb006ca38fcf
HecOmAC <malinchecoatl@gmail.com>
2026-04-23 05:52:03 +0000

    Creacion de rutas y finalizacion de la class FamilyStructure

--- src/app.py
+++ src/app.py
@@ -33,9 +33,37 @@ def sitemap():
 def handle_hello():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+
+    member = jackson_family.get_member(member_id)
+
+    if member is None:
+        return jsonify({"msg": "Member not found"}), 404
+
+    return jsonify(member), 200
+
+@app.route('/members', methods=['POST'])
+def add_member():
+
+    data = request.get_json()
+
+    new_member = jackson_family.add_member(data)
+
+    return jsonify(new_member), 200
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+
+    deleted = jackson_family.delete_member(member_id)
+
+    if not deleted:
+        return jsonify({"msg": "Member not found"}), 404
+
+    return jsonify({"done": True}), 200
+
 
 
 
